07cf0c31d79faa44a0f8e2bc3af62a92669c44f3
Patrick Ofilada <pcofilada@gmail.com>
2016-08-20 23:26:16 +0800

    Add services_controller

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,52 @@
+class ServicesController < ApplicationController
+
+  def index
+    @services = current_user.services
+  end
+
+  def new
+    @service = current_user.services.new
+  end
+
+  def create
+    @service = current_user.services.new(service_params)
+
+    if @service.save
+      flash[:success] = 'Service successfully added'
+      redirect_to services_path
+    else
+      flash[:error] = @service.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @service = current_user.services.find(params[:id])
+  end
+
+  def update
+    @service = current_user.services.find(params[:id])
+
+    if @service.update(service_params)
+      flash[:success] = 'Service successfully updated'
+      redirect_to services_path
+    else
+      flash[:error] =  @service.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @service = current_user.services.find(params[:id])
+    @service.destroy
+
+    redirect_to services_path
+  end
+
+private
+
+  def service_params
+    params.require(:service).permit(:name, :description, :notification_date, :frequency, :mobile_number, :custom_message)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
-  }
+  }, skip: [:sessions]
+  as :user do
+    delete 'users/sign_out' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
+  root 'home#index'
+  resources :services, except: :show
 end
